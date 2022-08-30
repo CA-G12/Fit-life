87602a09b62d5754d287589e852ed3c3402b933a
salsabeelomar <omarsalsabeel65@gmail.com>
2022-08-30 17:05:51 +0300

    link the function with indexes relates #19

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
-const {signUpPage,signUp}=require('../controllers')
+const {signUpPage,signUp,signInPage,signIn}=require('../controllers')
+
 const router = require('express').Router();
-router.post('/sign-up-form',signUp)
-router.get('/sign-up',signUpPage)
+router.post('/sign-up-form',signUp);
+router.post('/Sign-in-form',signIn);
+router.get('/sign-up',signUpPage);
+router.get('/Sign-in',signInPage);
 
 module.exports = router;

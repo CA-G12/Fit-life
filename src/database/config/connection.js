4bbe3d9ef278d147451dece5f37bafb3f10e3985
mohmmed23 <mhmmedben3@gmail.com>
2022-08-31 16:49:02 +0300

    fixed heroku #37

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -1,8 +1,33 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const { NODE_ENV, DATABASE_URL, DEV_DB_URL, TEST_DB_URL } = process.env;
+
+let connectionString = "";
+let ssl = false;
+console.log(NODE_ENV);
+switch (NODE_ENV) {
+  case "production":
+    connectionString = DATABASE_URL;
+    ssl = {
+      rejectUnauthorized: false,
+    };
+    break;
+  case "development":
+    connectionString = DEV_DB_URL;
+    break;
+
+  case "test":
+    connectionString = TEST_DB_URL;
+    break;
+
+  default:
+    throw new Error("invalid db url");
+}
+
 const connection = new Pool({
-    connectionString : process.env.DATABASE_URL,
-    ssl: false,
+  connectionString,
+  ssl,
 });
-module.exports = connection
+
+module.exports = connection;
